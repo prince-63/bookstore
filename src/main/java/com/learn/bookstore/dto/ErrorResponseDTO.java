3c0c49e9813e1a09b93281cf7b87060f5d64f49e
Prince Kumar Prasad <prince08833@gmail.com>
2025-07-09 14:11:15 +0530

    Enabled Global Exception Handlers

--- src/main/java/com/learn/bookstore/dto/ErrorResponseDTO.java
+++ src/main/java/com/learn/bookstore/dto/ErrorResponseDTO.java
@@ -1,8 +1,28 @@
 package com.learn.bookstore.dto;
 
 import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 @Builder
-public class ErrorResponseDTO {
+public record ErrorResponseDTO(String apiPath, HttpStatus errorCode, String errorMessage, LocalDateTime errorTime) {
 
 }
+
+//private  String apiPath;
+//
+//@Schema(
+//        description = "Error code representing the error happened"
+//)
+//private HttpStatus errorCode;
+//
+//@Schema(
+//        description = "Error message representing the error happened"
+//)
+//private  String errorMessage;
+//
+//@Schema(
+//        description = "Time representing when the error happened"
+//)
+//private LocalDateTime errorTime;
