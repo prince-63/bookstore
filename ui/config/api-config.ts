a6be22e7c84415c88231b52aa686ccb3c5ccb963
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-16 00:32:36 +0530

    added api configuration of user, auth, author, category

---
+++ ui/config/api-config.ts
@@ -0,0 +1,23 @@
+export default class ApiConfig {
+  static BASE_URL: string = 'http://localhost:8080';
+
+  // user
+  static USER_BASE: string = ApiConfig.BASE_URL + '/user';
+  static LOGIN: string = ApiConfig.USER_BASE + '/login';
+  static REGISTER_USER: string = ApiConfig.USER_BASE + '/register';
+  static GET_CURR_USER: string = ApiConfig.USER_BASE + '/get-curr';
+  static GET_ALL_USERS: string = ApiConfig.USER_BASE + '/get-all';
+  static DELETE_USER: string = ApiConfig.USER_BASE + '/delete';
+
+  // book
+  static BOOK_BASE = ApiConfig.BASE_URL + '/book';
+  static CREATE_CATEGORY: string = ApiConfig.BOOK_BASE + '/category';
+  static DELETE_CATEGORY: string = ApiConfig.BOOK_BASE + '/category';
+  static GET_ALL_CATEGORY: string = ApiConfig.BOOK_BASE + '/category/get/all';
+  static UPDATE_CATEGORY: string = ApiConfig.BOOK_BASE + '/category';
+
+  static ADD_AUTHOR: string = ApiConfig.BOOK_BASE + '/author';
+  static DELETE_AUTHOR: string = ApiConfig.BOOK_BASE + '/author';
+  static UPDATE_AUTHOR: string = ApiConfig.BOOK_BASE + '/author';
+  static GET_ALL_AUTHOR: string = ApiConfig.BOOK_BASE + '/author/get/all';
+}
